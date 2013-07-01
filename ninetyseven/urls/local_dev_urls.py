29f30eebd35875536517823fe8f914bb99d51958
Peter Baumgartner <pete@lincolnloop.com>
2013-06-30 21:27:19 -0500

    Serve static media without an extra server locally

--- ninetyseven/urls/local_dev_urls.py
+++ ninetyseven/urls/local_dev_urls.py
@@ -3,9 +3,17 @@ from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = patterns('',
+  # STATIC files
   url(
-    regex   = r'^(?P<path>.*)$',
-    view    = serve, 
+    regex   = r'^archetype/(?P<path>.*)$',
+    view    = serve,
+    name    = 'serve',
+    kwargs  = { 'document_root': settings.STATIC_ROOT, 'show_indexes': True, }
+  ),
+  # MEDIA files (user uploads)
+  url(
+    regex   = r'^ninetyseven/(?P<path>.*)$',
+    view    = serve,
     name    = 'serve',
     kwargs  = { 'document_root': settings.MEDIA_ROOT, 'show_indexes': True, }
   ),
